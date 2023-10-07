2f7b581475f48c80ff9500912e21edf63197d2cc
Aşkın Kesik <140115554+askinksk@users.noreply.github.com>
2023-10-07 16:08:49 +0300

    Add files via upload

---
+++ Controllers/BaseApiController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentTeacher.Service.Interfaces;
+
+namespace StudentTeacher.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        protected readonly IRepositoryManager _repository;
+        protected readonly ILoggerManager _logger;
+        protected readonly IMapper _mapper;
+
+        public BaseApiController(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = logger;
+            _mapper = mapper;
+        }
+    }
+}
